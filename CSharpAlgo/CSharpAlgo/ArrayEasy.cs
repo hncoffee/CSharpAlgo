933659a7b7062d8ca0de821e6ffde7776dd7cdc6
Hui He <small_hh@msn.com>
2020-04-03 21:23:50 -0700

    0403

--- CSharpAlgo/CSharpAlgo/ArrayEasy.cs
+++ CSharpAlgo/CSharpAlgo/ArrayEasy.cs
@@ -8,5 +8,82 @@ namespace CSharpAlgo
 {
     class ArrayEasy
     {
+        public int A136_SingleNumber(int[] nums)
+            {
+                if (nums.Length == 1) 
+        {
+            return nums[0];
+        }
+        Array.Sort(nums);
+         for (int i = 0; i < nums.Length; i++) 
+         {           
+                if (nums[i] != nums[i + 1])
+                {
+                    return nums[i];
+                }
+                else
+                {
+                    i++;
+                     if(i+1==nums.Length-1)
+                        return nums[i+1];
+                }
+           
+        }
+        return -1;
+        }
+
+        public bool A202_IsHappy(int n) 
+        {
+            var visited = new Dictionary<int, bool>();
+            var next = n;
+
+            while (!visited.ContainsKey(next))
+                {
+                visited.Add(next,true);
+                var start = next;
+                next =0;
+                while (start > 0)
+                {
+                    next += (start % 10) * ( start % 10);
+                    start = start / 10;
+                }
+
+                if (next == 1) return true;
+            }
+
+            return false;
+                }
+
+
+        public int A53_MaxSubArray(int[] nums) {
+        int size = nums.Length; 
+        int max_so_far = int.MinValue,  
+            max_ending_here = 0; 
+  
+        for (int i = 0; i < size; i++) 
+        { 
+            max_ending_here += nums[i]; 
+              
+            if (max_so_far < max_ending_here) 
+                max_so_far = max_ending_here; 
+              
+            if (max_ending_here < 0) 
+                max_ending_here = 0; 
+        } 
+          
+        return max_so_far; 
+        
     }
-}
+
+        }
+
+    
+
+
+         
+           
+
+    }
+
+        
+ 
--- CSharpAlgo/CSharpAlgo/Program.cs
+++ CSharpAlgo/CSharpAlgo/Program.cs
@@ -10,6 +10,15 @@ namespace CSharpAlgo
     {
         static void Main(string[] args)
         {
+            //int[] a = {1,2,2};
+            int n=19;
+            int [] a = {-2,1,-3,4,-1,2,1,-5,4}; 
+
+            ArrayEasy test = new ArrayEasy();
+            int t = test.A53_MaxSubArray(a);
+            Console.WriteLine(t);
+            Console.Read();
+
         }
     }
 }
